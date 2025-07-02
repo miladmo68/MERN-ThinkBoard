b9f5b62672fa3eeaa7cd7ecb8bde288d10ad7d24
miladmo68 <miladmo68@gmail.com>
2025-07-02 12:10:17 -0400

    Done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from "../components/RateLimitedUI";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 import api from "../lib/axios";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -34,6 +35,8 @@ const HomePage = () => {
         {loading && (
           <div className="text-center text-primary py-10">Loading notes</div>
         )}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
         {notes.length > 0 && !isRateLimited && (
           <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
